eb7978c2ce4e75d715d956b8103a2a1c3a34e70f
Timmy Garrabrant <timmy.garrabrant@gmail.com>
2018-01-16 16:28:13 -0700

    stuff

--- storybook/stories/CKEditor/index.ios.js
+++ storybook/stories/CKEditor/index.ios.js
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 import { WebView } from "react-native";
 const ckeditor = require("./ckeditor.html");
 
-const Button = props => <WebView source={ckeditor} style={{ flex: 1 }} />;
+const Button = props => <WebView source={ckeditor} 
+onMessage={(data)=>console.warn(data)}
+style={{ flex: 1 }} />;
 
 Button.propTypes = {
   // children: PropTypes.node.isRequired,
